bf8e128e3f26e23ff34755eb7f7118c2f22887a1
sdhaiddder8 <49123449+sdhaiddder8@users.noreply.github.com>
2026-08-13 09:54:38 +0300

    replace turbo with base react plugin

--- ekey-react-native-sdk/android/src/main/java/com/example/ekeysdk/EkeySdkPackage.kt
+++ ekey-react-native-sdk/android/src/main/java/com/example/ekeysdk/EkeySdkPackage.kt
@@ -1,12 +1,12 @@
 package com.example.ekeysdk
 
-import com.facebook.react.TurboReactPackage
+import com.facebook.react.BaseReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.module.model.ReactModuleInfo
 import com.facebook.react.module.model.ReactModuleInfoProvider
 
-class EkeySdkPackage : TurboReactPackage() {
+class EkeySdkPackage : BaseReactPackage() {
     override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule? =
         if (name == EkeySdkModule.NAME) EkeySdkModule(reactContext) else null
 
